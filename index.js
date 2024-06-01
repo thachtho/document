a417a255aaeec1dde6e0b12fa8a6d7a5b11d2207
thobui14ig <buithanhtho14ig@gmail.com>
2024-06-01 14:24:46 +0700

    change port

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.get('/:directoryName/', (req, res) => {
     });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
