c2291ada3f26961559755053e11cd91cd1c1201f
thobui14ig <buithanhtho14ig@gmail.com>
2024-06-01 15:39:40 +0700

    ok

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ const publicDirectoryPath = path.join(__dirname, 'public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.get('/iframe', (req, res) => {
-    res.send(`<iframe src="http://103.214.10.223:3001" width="600" height="400" frameborder="0"></iframe>
-    </body>`)
-})
-
 // Middleware để xử lý route '/'
 app.get('/', (req, res) => {
     // Đọc thư mục và tạo danh sách các thư mục và tệp
@@ -29,9 +24,9 @@ app.get('/', (req, res) => {
             // Kiểm tra xem đối tượng là thư mục hay tệp
             const isDirectory = fs.statSync(path.join(publicDirectoryPath, file)).isDirectory();
             if (isDirectory) {
-                fileList += `<li style="margin-bottom: 10px"><a href="/${file}/">${file}</a></li>`;
+                fileList += `<li style="margin-bottom: 10px"><a href="/document/${file}/">${file}</a></li>`;
             } else if (path.extname(file) === '.md') {
-                fileList += `<li style="margin-bottom: 10px"><a href="/file/${encodeURIComponent(file)}">${file}</a></li>`;
+                fileList += `<li style="margin-bottom: 10px"><a href="/document/file/${encodeURIComponent(file)}">${file}</a></li>`;
             }
         });
         fileList += '</ol>';
@@ -89,9 +84,9 @@ app.get('/:directoryName/', (req, res) => {
             // Kiểm tra xem đối tượng là thư mục hay tệp
             const isDirectory = fs.statSync(path.join(directoryPath, file)).isDirectory();
             if (isDirectory) {
-                fileList += `<li style="margin-bottom: 10px"><a href="/${directoryName}/${file}/">${file}</a></li>`;
+                fileList += `<li style="margin-bottom: 10px"><a href="/document/${directoryName}/${file}/">${file}</a></li>`;
             } else if (path.extname(file) === '.md') {
-                fileList += `<li style="margin-bottom: 10px"><a href="/file/${encodeURIComponent(directoryName + '/' + file)}">${file}</a></li>`;
+                fileList += `<li style="margin-bottom: 10px"><a href="/document/file/${encodeURIComponent(directoryName + '/' + file)}">${file}</a></li>`;
             }
         });
         fileList += '</ol>';
